0a492c8894f1c6738eaea8dfe47a35efc0e01fa2
aripzainul <aripzainul16@gmail.com>
2021-03-14 14:46:40 +0700

    First push

---
+++ app/Views/hello/world.php
@@ -0,0 +1,8 @@
+<?= $this->extend('layout') ?>
+
+<?= $this->section('content') ?>
+
+  <h3>Hello Guys</h3>
+  <?= $data; ?> // cara menangkap parsingan data dari controller
+
+<?= $this->endSection() ?>
